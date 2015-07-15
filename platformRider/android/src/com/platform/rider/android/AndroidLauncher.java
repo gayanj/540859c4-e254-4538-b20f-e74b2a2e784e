1c576b05c822fba2c17d0761f25dc792d057346d
gayanj <gayanj@zone24x7.com>
2015-07-15 23:44:13 +0530

    Added Google Ads support!

--- platformRider/android/src/com/platform/rider/android/AndroidLauncher.java
+++ platformRider/android/src/com/platform/rider/android/AndroidLauncher.java
@@ -1,15 +1,43 @@
 package com.platform.rider.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.platform.rider.main.AnyDirection;
 
 public class AndroidLauncher extends AndroidApplication {
+    private static final String AD_UNIT_ID = "ca-app-pub-8464762813805843/8327434010";
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new AnyDirection(), config);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+        View gameView = initializeForView(new AnyDirection(), config);
+        AdView adView = new AdView(this);
+        AdSize adSize = new AdSize(300,30);
+        adView.setAdSize(adSize);
+        adView.setAdUnitId(AD_UNIT_ID);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        RelativeLayout layout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams params =
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        layout.addView(gameView);
+        layout.addView(adView, params);
+        setContentView(layout);
 	}
 }
